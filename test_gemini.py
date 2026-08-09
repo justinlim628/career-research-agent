4d495cfecd7c97e9b369a8e2d925bf7d07e8811b
Justin Lim <justinlim628@gmail.com>
2026-08-09 18:36:14 -0400

    formatting changes

--- test_gemini.py
+++ test_gemini.py
@@ -1,8 +1,5 @@
-import google.generativeai as genai
-import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 
 load_dotenv()
 # genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -11,6 +8,6 @@ load_dotenv()
 #     if 'generateContent' in model.supported_generation_methods:
 #         print(model.name)
 
-llm = ChatGoogleGenerativeAI(model='gemini-flash-latest')
-response = llm.invoke('say hello in one sentence.')
+llm = ChatGoogleGenerativeAI(model="gemini-flash-latest")
+response = llm.invoke("say hello in one sentence.")
 print(response.content)
